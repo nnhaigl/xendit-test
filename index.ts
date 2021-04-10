9bc7dbd2f4798399fb375455c3b4581ea735485d
Hai Nguyen <nnhaigl@gmail.com>
2021-04-10 23:18:29 +0700

    add charactercontroller into index to export api

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
+import { CharacterController } from './src/modules/character/character.controller';
 import App from './src/app';
 require('dotenv').config()
 
 async function active() {
   const port = +process.env.PORT || 3000
   const app = new App(
-    []
+    [
+      CharacterController
+    ]
   );
   await app.listen(port).then(() => {
     console.log(`App listening on port ${port}`)
